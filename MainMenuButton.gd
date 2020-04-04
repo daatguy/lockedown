033117830ec9df0f7f4f1564d8895d3b44c19341
daatguy <daatguy@outlook.com>
2020-04-04 02:16:12 -0400

    Button sounds

--- MainMenuButton.gd
+++ MainMenuButton.gd
@@ -1,5 +1,8 @@
 extends Button
 
+onready var hoverSound = get_node("Hover")
+const vol = -48;
+const gain = 4;
 const shakeGrow = 0.025;
 const shakeMin = 3;
 const shakeMax = 24;
@@ -13,11 +16,17 @@ func _ready():
 
 func _process(delta):
 	if(self.is_hovered()):
+		hoverSound.playing = true;
+		hoverSound.pitch_scale = 1+(shakeSize-shakeMin)/shakeMax;
+		hoverSound.volume_db = vol+gain*(shakeSize-shakeMin)/shakeMax;
 		self.rect_global_position.x = randf()*shakeSize-shakeSize/2+x;
 		self.rect_global_position.y = randf()*shakeSize-shakeSize/2+y;
 		if(shakeSize<shakeMax):
 			shakeSize += shakeGrow;
 	else:
+		hoverSound.playing = false;
+		hoverSound.pitch_scale = 1
+		hoverSound.volume_db = vol;
 		shakeSize = shakeMin;
 		self.rect_global_position.x = x;
 		self.rect_global_position.y = y;

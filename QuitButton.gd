0a8dc0e97d69dcb7548c577d6ea5b0a01d4fa4d2
daatguy <daatguy@outlook.com>
2020-04-04 04:14:41 -0400

    Added tileset

--- QuitButton.gd
+++ QuitButton.gd
@@ -2,6 +2,7 @@ extends "res://MainMenuButton.gd"
 
 func _ready():
 	._ready()
+# warning-ignore:return_value_discarded
 	get_node("../Options").connect("options_displayed", self, "_on_options_displayed")
 
 func _get_max_text():

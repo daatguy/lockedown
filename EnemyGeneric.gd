2e08facfcb5269c6f072c16211361a313436ea34
Kai Hoop <unrelatedstring@gmail.com>
2020-04-05 02:37:38 -0400

    His coconut gun can fire in spurts

--- EnemyGeneric.gd
+++ EnemyGeneric.gd
@@ -1,14 +1,16 @@
 extends RigidBody2D
 var ticks = 0 #make time in future?
 export (PackedScene) var Bullet
-var ticks_per_shot = 30
+export var ticks_per_shot = 30
 
 func _process(delta): #everything else ignores delta ¯\_(ツ)_/¯
 	ticks += 1
 	if ticks > ticks_per_shot:
-		print("shot")
 		ticks %= ticks_per_shot
-		var bullet = Bullet.instance()
-		$"..".add_child(bullet)
-		bullet.position = position
-		bullet.velocity = Vector2(25, -25)
+		shoot(Vector2(25, -25))
+
+func shoot(v):
+	var bullet = Bullet.instance()
+	$"..".add_child(bullet)
+	bullet.position = position
+	bullet.velocity = v

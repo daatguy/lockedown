6bf8c62969a4c887659bcdc130ca60cfbda02566
Kai Hoop <unrelatedstring@gmail.com>
2020-04-05 02:29:11 -0400

    they do be firing doe

--- EnemyGeneric.gd
+++ EnemyGeneric.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 var ticks = 0 #make time in future?
 export (PackedScene) var Bullet
-var ticks_per_shot = 15
+var ticks_per_shot = 30
 
 func _process(delta): #everything else ignores delta ¯\_(ツ)_/¯
 	ticks += 1
 	if ticks > ticks_per_shot:
+		print("shot")
 		ticks %= ticks_per_shot
 		var bullet = Bullet.instance()
-		add_child(bullet)
-		bullet.velocity = Vector2(50, -50)
+		$"..".add_child(bullet)
+		bullet.position = position
+		bullet.velocity = Vector2(25, -25)

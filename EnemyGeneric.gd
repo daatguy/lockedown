c4dbc0947d82648cc5e1f4f5f29d421d75ff470c
Kai Hoop <unrelatedstring@gmail.com>
2020-04-05 04:53:28 -0400

    Added more shooty code

--- EnemyGeneric.gd
+++ EnemyGeneric.gd
@@ -7,17 +7,18 @@ func _process(delta):
 	time += delta
 	if time > seconds_per_shot:
 		time = fmod(time, seconds_per_shot)
-		shoot(Vector2(-300, 300))
-		shoot(Vector2(300, 300))
-		shoot(Vector2(-300, -300))
-		shoot(Vector2(300, -300))
-		shoot(Vector2(0, 300))
-		shoot(Vector2(0, -300))
-		shoot(Vector2(-300, 0))
-		shoot(Vector2(300, 0))
+		shoot_angle(300, angle_to_player())
 
 func shoot(v):
 	var bullet = Bullet.instance()
 	$"..".add_child(bullet)
 	bullet.position = position
 	bullet.velocity = v
+
+func shoot_angle(speed, angle):
+	var v = Vector2(speed, 0)
+	v = v.rotated(angle)
+	shoot(v)
+	
+func angle_to_player():
+	return PI + position.angle_to_point($"../Player".position)

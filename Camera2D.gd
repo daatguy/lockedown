22bb7ed9d1d6b023772a266eeb4b7ef6c056c0e1
daatguy <daatguy@outlook.com>
2020-04-04 20:29:18 -0400

    camera shake and fucked up screen movement

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,16 +1,19 @@
 extends Camera2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var shakeAmount = 0;
+var shakeDecay = 0.6;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	shakeAmount = 32;
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	shakeAmount *= shakeDecay;
+	var shakeAmount = Vector2();
+	shakeAmount.x = (randf()-0.5)*shakeAmount;
+	shakeAmount.y = (randf()-0.5)*shakeAmount;
+	offset = shakeAmount;
+	pass

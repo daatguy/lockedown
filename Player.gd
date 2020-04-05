85fd219b15196fd3c2b148e648cad8c9b7000ec5
daatguy <daatguy@outlook.com>
2020-04-05 01:23:07 -0400

    Player walk and idle anims

--- Player.gd
+++ Player.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
-export var speed = 400;
+export var speed = 800;
+var direction = 0;
 var screen_size #this feels like it belongs somewhere else
 onready var fog = get_node("Camera2D/Fog")
+onready var sprite = get_node("AnimatedSprite")
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -19,5 +21,12 @@ func _process(delta):
 		velocity.y -= 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+		direction = fposmod(floor(rad2deg(-velocity.angle())/45),8);
+		if(velocity.y>0 || (velocity.y==0 && velocity.x<0)):
+			direction += 1
+		print(direction);
+		sprite.animation = "walk"+str(direction)
+	else:
+		sprite.animation = "idle"+str(direction)
 	if(!test_move(transform,velocity * delta)):
-		position += velocity * delta;
+		position += velocity * delta

b4cf5024f08fce0edbb571e134bbf6c6d1f632e7
Painfulapple <36847540+Painfulapple@users.noreply.github.com>
2020-04-05 17:16:37 -0400

    Update EnemyStar.gd

--- EnemyStar.gd
+++ EnemyStar.gd
@@ -9,3 +9,7 @@ func shoot_pattern():
 	shoot(Vector2(-300,-300), 1200, 37)
 	shoot(Vector2(0,-300), 1200, 37)
 	shoot(Vector2(300,-300), 1200, 37)
+
+func move_pattern():
+	chase(0);
+	#This is just a placeholder until we come up with movement patterns for these enemies

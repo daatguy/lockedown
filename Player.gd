bee85c0d038f373cd0fb71b8f4382fcafa4ace2a
Kai Hoop <unrelatedstring@gmail.com>
2020-04-05 19:49:38 -0400

    raycast doesn't work but hey whatever

--- Player.gd
+++ Player.gd
@@ -11,6 +11,7 @@ var firing = .25; #how many seconds it takes you to shoot a bullet
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	$RayCast2D.add_exception(self)
 
 func _process(delta):
 	if(reload < firing):
@@ -45,8 +46,20 @@ func _on_Bullet_hit(damage):
 	
 func _input(event):
 	if event is InputEventMouseButton && reload > firing:
-		shoot_angle(1000, position.angle_to_point(get_global_mouse_position()),500,1)
+		var angle = position.angle_to_point(get_global_mouse_position())
+		#shoot_angle(1000, angle, 500,1)
+		shoot_raycast(angle)
 		reload = 0
+		
+func shoot_raycast(angle):
+	var to = Vector2(500, 0)
+	to = to.rotated(angle)
+	$RayCast2D.cast_to = to
+	$RayCast2D.force_raycast_update()
+	if ($RayCast2D.is_colliding()):
+		print("hitted")
+	else:
+		print("miss")
 		
 func shoot(v, reach, damage):
 	var bullet = PlayerBullet.instance()

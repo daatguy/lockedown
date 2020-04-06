85de0846c16c3925f953217ba4ec6cdc64b62961
daatguy <daatguy@outlook.com>
2020-04-05 21:02:52 -0400

    dd

--- Ray.gd
+++ Ray.gd
@@ -5,3 +5,7 @@ var timer = 0;
 func _process(delta):
 	timer =+ delta
 	$"Sprite".modulate = Color(1,1,1,1.1-timer*4)
+
+func _on_self_body_entered(body):
+	if body != $"../Player":
+		body.get_parent().remove_child(body);
